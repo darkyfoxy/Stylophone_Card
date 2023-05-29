7526f18ba3dd03327c6da19b290b37654e45c131
Vladislav <51966677+darkyfoxy@users.noreply.github.com>
2023-05-29 17:07:36 +0300

    gosts

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,36 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
+  * @file       main.c
+  * @brief      This is the top layer file.
+  *
+  * @author     darkyfoxy [*GitHub*](https://github.com/darkyfoxy)
+  * @version    0.01
+  * @date       05.05.2023
+  *
   ******************************************************************************
-  * @attention
+  * @copyright  <h3>Copyright (c) 2023 Pavlov V.</h3>
   *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
   *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
   ******************************************************************************
   */
+
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
